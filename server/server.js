db96f34287ae6a2104698bdbaa7e38885b6cb43b
Saksham-Khanna <68804085+Saksham-Khanna@users.noreply.github.com>
2026-03-09 11:08:14 +0530

    Fixed deployment issues

---
+++ server/server.js
@@ -0,0 +1,6 @@
+/**
+ * Render deployment shim
+ * This file allows the project to run with Render's default 'node server.js' command
+ * and also serves as a cleaner entry point.
+ */
+require('./api/server.js');
